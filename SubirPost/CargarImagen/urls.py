c5869cf0a1e1c5e8f0754f47038b1fe757aa4da3
Hristo Chavez <hristoandychavezhuertas@gmail.com>
2024-03-09 13:18:05 -0500

    Mostrar imagenes de los post

--- SubirPost/CargarImagen/urls.py
+++ SubirPost/CargarImagen/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from CargarImagen.views import show_posts
+from django.conf import settings
+from django.contrib.staticfiles.urls import static
 
 urlpatterns = [
     path('', show_posts)
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- SubirPost/SubirPost/settings.py
+++ SubirPost/SubirPost/settings.py
@@ -11,9 +11,14 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Configuración para mostrar imagenes.
+MEDIA_ROOT = os.path.join(BASE_DIR, '')
+MEDIA_URL = ''
 
 
 # Quick-start development settings - unsuitable for production
